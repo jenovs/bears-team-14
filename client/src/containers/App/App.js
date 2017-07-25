18d963a28f8c332aa76e838153b3218f769559fd
Viktors Jenovs <jviktors@gmail.com>
2017-07-25 09:30:00 +0300

    Wrap App component in React-Router

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,4 +1,12 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Link,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
 import './App.css';
 import api from '../../fakeApi';
 
@@ -30,28 +38,39 @@ class App extends Component {
   render() {
     const { data } = this.state;
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome</h2>
-        </div>
-        <div className="App-intro">
-          {!!data.length
-            ? data.map((item, i) =>
-                <div key={item.id}>
-                  <p>
-                    <strong>
-                      {item.companyName}
-                    </strong>
-                    {' - '}
-                    <span>
-                      {item.info.title}
-                    </span>
-                  </p>
-                </div>
-              )
-            : <div>Loading...</div>}
-        </div>
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/about" component={() => <div>About page</div>} />
+          <Route exact path="/">
+            <div className="App">
+              <div className="App-header">
+                <h2>Welcome</h2>
+                <Link to="/about" style={{ color: 'white' }}>
+                  About
+                </Link>
+              </div>
+              <div className="App-intro">
+                {!!data.length
+                  ? data.map((item, i) =>
+                      <div key={item.id}>
+                        <p>
+                          <strong>
+                            {item.companyName}
+                          </strong>
+                          {' - '}
+                          <span>
+                            {item.info.title}
+                          </span>
+                        </p>
+                      </div>
+                    )
+                  : <div>Loading...</div>}
+              </div>
+            </div>
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </Router>
     );
   }
 }
