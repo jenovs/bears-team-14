2ebd97fc93d9ceb995a388c171d976474e68a930
Nikolajus Elkana Eimutis <niko.eimutis.niko1@gmail.com>
2017-07-31 17:02:04 +0300

    Fixed merge issues

--- server/controllers/JobsHandler.js
+++ server/controllers/JobsHandler.js
@@ -1,20 +1,11 @@
 const Job = require('../models/Job.js');
 
 module.exports = {
-<<<<<<< HEAD
   getAll: async function(req, res) {
       let jobs = await Job.find();
 
       if (!jobs) throw 'No jobs found!'; 
 
       res.json(jobs);
-=======
-  getAll: function(req, res) {
-    Job.find().exec((err, jobs) => {
-      if (err) throw err;
-
-      res.json(jobs);
-    });
->>>>>>> upstream/development
   },
 };
