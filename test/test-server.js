c76f32c33b896220e33b12f4c2a1dfb497238b79
Zyberg <niko.eimutis.niko1@gmail.com>
2017-08-03 16:08:25 +0300

    Tests use async/await instead of callbacks

--- test/test-server.js
+++ test/test-server.js
@@ -10,8 +10,9 @@ const Job = require('../server/models/Job.js');
 const should = chai.should();
 chai.use(chaiHttp);
 
+
 describe('Jobs', function() {
-  beforeEach(function(done) {
+  beforeEach(async function() {
     const newJob = new Job({
       companyName: 'Company Name',
       info: {
@@ -24,48 +25,43 @@ describe('Jobs', function() {
       tags: ['Job', 'Tags'],
       expDate: new Date('October 13, 1975 11:13:00'),
     });
-    newJob.save(function(err) {
-      done();
-    });
+    await newJob.save();
   });
 
-  afterEach(function(done) {
-    Job.collection.drop();
-    done();
+  afterEach(async function() {
+    await Job.collection.drop();
   });
 
   describe('#GET ALL', function() {
-    it('should list ALL jobs on /jobs GET', function(done) {
-      chai.request(server).get('/api/v1/jobs').end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('array');
-        res.body[0].should.have.property('_id');
-        res.body[0].should.have.property('companyName');
-        res.body[0].should.have.property('info').with.property('title');
-        res.body[0].should.have.property('info').with.property('description');
-        res.body[0].should.have.property('info').with.property('imgUrl');
-        res.body[0].should.have.property('info').with.property('website');
-        res.body[0].should.have.property('location');
-        res.body[0].should.have.property('tags');
-        res.body[0].should.have.property('expDate');
-        res.body[0].companyName.should.equal('Company Name');
-        res.body[0].info.title.should.equal('Job Title');
-        res.body[0].info.description.should.equal('Job Description');
-        res.body[0].info.imgUrl.should.equal('Job Image URL');
-        res.body[0].info.website.should.equal('Job Website');
-        res.body[0].location.should.equal('Job Location');
-        res.body[0].tags.should.deep.equal(['Job', 'Tags']);
-        new Date(res.body[0].expDate).should.deep.equal(
-          new Date('October 13, 1975 11:13:00')
-        );
-        done();
-      });
+    it('should list ALL jobs on /jobs GET', async function() {
+      let res = await chai.request(server).get('/api/v1/jobs');
+	  res.should.have.status(200);
+	  res.body.should.be.a('array');
+	  res.body[0].should.have.property('_id');
+	  res.body[0].should.have.property('companyName');
+	  res.body[0].should.have.property('info').with.property('title');
+	  res.body[0].should.have.property('info').with.property('description');
+	  res.body[0].should.have.property('info').with.property('imgUrl');
+	  res.body[0].should.have.property('info').with.property('website');
+	  res.body[0].should.have.property('location');
+	  res.body[0].should.have.property('tags');
+	  res.body[0].should.have.property('expDate');
+	  res.body[0].companyName.should.equal('Company Name');
+	  res.body[0].info.title.should.equal('Job Title');
+	  res.body[0].info.description.should.equal('Job Description');
+	  res.body[0].info.imgUrl.should.equal('Job Image URL');
+	  res.body[0].info.website.should.equal('Job Website');
+	  res.body[0].location.should.equal('Job Location');
+	  res.body[0].tags.should.deep.equal(['Job', 'Tags']);
+	  new Date(res.body[0].expDate).should.deep.equal(
+	    new Date('October 13, 1975 11:13:00')
+	  );
     });
   });
 
   describe('#POST', function() {
-    it('should add a SINGLE new job on /jobs/create POST', function(done) {
-      chai
+    it('should add a SINGLE new job on /jobs/create POST', async function() {
+      let res = await chai
         .request(server)
         .post('/api/v1/jobs/create')
         .send({
@@ -77,40 +73,24 @@ describe('Jobs', function() {
           location: 'Job2 Location',
           tags: 'Second, Job, Tags',
           expDate: 'October 20, 1975 11:13:00',
-        })
-        .end(function(err, res) {
-          res.should.have.status(201);
-          done();
         });
+      res.should.have.status(201);
     });
   });
 
   describe('#PUT', function() {
-    it('should update a SINGLE job on /job/<id> PUT', function(done) {
-      chai.request(server).get('/api/v1/jobs').end(function(err, res) {
-        chai
-          .request(server)
-          .put('/api/v1/job/' + res.body[0]._id)
-          .send({ companyName: 'Google' })
-          .end(function(err, res) {
-            res.should.have.status(204);
-            done();
-          });
-      });
+    it('should update a SINGLE job on /job/<id> PUT', async function() {
+      let jobs = await chai.request(server).get('/api/v1/jobs');
+      let res = await chai.request(server).put('/api/v1/job/' + jobs.body[0]._id).send({ companyName: 'Google' });
+      res.should.have.status(204);
     });
   });
 
   describe('#DELETE', function() {
-    it('should delete a SINGLE job on /job/<id> DELETE', function(done) {
-      chai.request(server).get('/api/v1/jobs').end(function(err, res) {
-        chai
-          .request(server)
-          .delete('/api/v1/job/' + res.body[0]._id)
-          .end(function(err, res) {
-            res.should.have.status(204);
-            done();
-          });
-      });
+    it('should delete a SINGLE job on /job/<id> DELETE', async function() {
+      let jobs = await chai.request(server).get('/api/v1/jobs');
+      let res = await chai.request(server).delete('/api/v1/job/' + jobs.body[0]._id);
+      res.should.have.status(204);
     });
   });
 });
